3f0661f805a951826ae1ea3a2ba79c715121cb21
Christopher Henderson <chenderson5280@gmail.com>
2020-10-04 20:05:03 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+sum = num1+num2
+sum
 end
 
 def subtraction(num1, num2)
-
+  sum = num1-num2
+  sum
 end
 
 def division(num1, num2)
-
+total = num1 / num2
+total
 end
 
 def multiplication(num1, num2)
-
+  total = num1 * num2
+  total
 end
 
 def modulo(num1, num2)
-
+total = num1.modulo(num2)
+total
 end
 
 def square_root(num)
+ Math.sqrt(num)
 
 end
